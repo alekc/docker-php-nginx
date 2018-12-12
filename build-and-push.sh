9ca14200b84f7817ff10efee663a57efbfb31e4a
Alexander Chernov <alekcander@gmail.com>
2018-12-12 17:27:13 +0000

    typo in name parameter.

--- build-and-push.sh
+++ build-and-push.sh
@@ -9,7 +9,7 @@ echo "Building docker image ${IMAGE_NAME}"
 docker build --build-arg PHP_VERSION=${PHP_VERSION} -t ${IMAGE_NAME}:latest .
 if [ $? -eq 0 ]; then
     #docker run --rm -it --entrypoint=/bin/ash ${IMAGE_NAME}:latest -i && exit
-    docker run --rm -d -name=testing-build ${IMAGE_NAME}:latest
+    docker run --rm -d --name=testing-build ${IMAGE_NAME}:latest
 #    docker push alekcander/php-nginx:latest .
 fi
 
